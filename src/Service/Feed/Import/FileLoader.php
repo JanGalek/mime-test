675b459b60629ae9171d4f015c1fe6dad1470313
Jan Galek <galek@1webit.cz>
2024-06-29 11:23:01 +0200

    Add Data

---
+++ src/Service/Feed/Import/FileLoader.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Mime\Service\Feed\Import;
+
+interface FileLoader
+{
+
+}
---
+++ src/Service/Feed/Import/JsonLoader.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mime\Service\Feed\Import;
+
+use Mime\Service\Feed\Import\FileLoader;
+
+class JsonLoader implements FileLoader
+{
+
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
+}
